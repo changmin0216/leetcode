0840af0b2b7a50ad154311019860190b705b0659
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-03-20 19:45:44 +0900

    Time: 90 ms (92.3%), Space: 21.5 MB (94.59%) - LeetHub

--- 2478-longest-nice-subarray/2478-longest-nice-subarray.py
+++ 2478-longest-nice-subarray/2478-longest-nice-subarray.py
@@ -4,10 +4,9 @@ class Solution(object):
     def longestNiceSubarray(self, nums):
         answer = j = mask = 0
         for i, x in enumerate(nums):
-            while mask & x:
+            while mask & x: # 1이 아니면
                 mask ^= nums[j]
-                j += 1
-            answer = max(answer, i - j + 1)
-            
+                j+=1
+            answer = max(answer, i-j+1)
             mask |= x
         return answer
