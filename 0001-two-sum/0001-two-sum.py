0f714eed99148ef897054d2ad91f5e6e29d2fc8c
changmin0216 <105960247+changmin0216@users.noreply.github.com>
2025-04-01 14:18:47 +0900

    Time: 2594 ms (6.48%), Space: 19.3 MB (6.42%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,9 @@
-from itertools import combinations
-class Solution(object):
-    def twoSum(self, nums, target):
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         l = []
         for i, x in enumerate(nums):
             l.append((i, x))
-        for k in combinations(l, 2):
-            if k[0][1] + k[1][1] == target:
-                return [k[0][0], k[1][0]]
+
+        for i in combinations(l, 2):
+            if i[0][1] + i[1][1] == target:
+                return [i[0][0], i[1][0]] 
